eecfce37555aca4b408d8c218d8221ec2be8377c
Hsu Mon Kyaw <hsumonkyaw@ubuntu.ubuntu-domain>
2020-10-04 10:35:17 -0400

    template.html

---
+++ bootstrap/js/template.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function(){
+	document.getElementById("btn1").onmouseover=()=>{
+	document.getElementById("btn1").style.backgroundColor ="white";	
+	}
+	document.getElementById("btn1").onmouseout=()=>{
+	document.getElementById("btn1").style.backgroundColor ="#33bbff";	
+	}
+	document.getElementById("btn2").onmouseover=()=>{
+	document.getElementById("btn2").style.backgroundColor ="white";	
+	}
+	document.getElementById("btn2").onmouseout=()=>{
+	document.getElementById("btn2").style.backgroundColor ="#33bbff";	
+	}
+	document.getElementById("btn4").onmouseover=()=>{
+	document.getElementById("btn4").style.backgroundColor ="white";	
+	}
+	document.getElementById("btn4").onmouseout=()=>{
+	document.getElementById("btn4").style.backgroundColor ="#33bbff";	
+	}
+	document.getElementById("btn3").onmouseover=()=>{
+	document.getElementById("btn3").style.backgroundColor ="#33bbff";	
+	}
+	document.getElementById("btn3").onmouseout=()=>{
+	document.getElementById("btn3").style.backgroundColor ="white";	
+	}
+	document.getElementById("more").onmouseover=()=>{
+	document.getElementById("more").style.backgroundColor ="white";	
+	}
+	document.getElementById("more").onmouseout=()=>{
+	document.getElementById("more").style.backgroundColor ="#0099ff";	
+	}
+})
